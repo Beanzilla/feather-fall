09333d71ed81593d3ad4aa8b2d3b8ef1a56290ac
magus-vageta-fusion <123661493+magus-vageta-fusion@users.noreply.github.com>
2023-01-28 19:53:43 -0700

    Update init.lua
    Cleaned up code and removed some notes. Also, I figured out how to smooth out the slow falling by turning off gravity, making slow falls a lot more stable. Also added options to control the added features.

--- init.lua
+++ init.lua
@@ -1,5 +1,11 @@
 
 local ff = {}
+local current_y = 0
+local gravity_reset_timer = -1
+local use_gravity_calls = minetest.settings:get_bool("feather_fall.use_gravity_calls", true) -- a switch to disable change gravity calls
+local falling_speed = math.abs(tonumber(minetest.settings:get("feather_fall.falling_speed")) or 6)*-1
+local holding_requirement = minetest.settings:get("feather_fall.holding_requirement") or "hotbar"
+local hotbar_slots = tonumber(minetest.settings:get("feather_fall.hotbar_slots")) or 8
 
 ff.VERSION = "1.0.2"
 
@@ -19,23 +25,21 @@ function ff.game_mode()
 end
 
 function ff.set_speed(user)
-    local current_y = user:get_velocity()["y"]
+	local last_y = current_y
+    current_y = user:get_velocity()["y"] -- returns float
     -- If we aren't going negative floating isn't needed
-    if current_y >= 0 then return end
-    -- Ok we are negative
-    --minetest.chat_send_player(user:get_player_name(), "Y: "..tostring(current_y))
-    current_y = math.abs(current_y) -- Flip it so we can use it sanely
-    if current_y > 3 then -- Only activate if greater than -2
-        user:add_velocity({x=0, y=current_y*0.20, z=0}) -- Reduce speed by X percent (it appears don't go past 0.50)
+    if current_y > 0 then return end
+	if (current_y < 0 and use_gravity_calls == true) then gravity_reset_timer = 2 end
+    if current_y < falling_speed then
+        user:add_velocity({x=0, y=current_y*-0.2, z=0}) -- Reduce speed by X percent (it appears don't go past 0.50)
+		if use_gravity_calls == true then user:set_physics_override({gravity=0}) end
     end
 end
 
--- this function is part of the hotbar code, at line 53.
 function ff.find_stack(inv, listname, find_stack)
 	local max_size = inv:get_size(listname)
-	local i = 0
 	local RETURN_ME = -1
-	while i < max_size do
+	for i = 0, max_size do
 		local stack = inv:get_stack(listname, i)
 		i = i+1
 		if stack:get_name() == find_stack then
@@ -47,28 +51,32 @@ function ff.find_stack(inv, listname, find_stack)
 end
 
 minetest.register_globalstep(function (dtime)
+	if gravity_reset_timer > -1 then gravity_reset_timer = gravity_reset_timer-1 end
     for _, player in ipairs(minetest.get_connected_players()) do
-	local inv = player:get_inventory()
-	-- minetest.chat_send_all("Y: "..tostring(ff.find_stack(inv, "main", "feather_fall:feather")))
-	--  HOTBAR CODE:
-	local seek_stack = ff.find_stack(inv, "main", "feather_fall:feather")
-	if (seek_stack <= 8 and seek_stack ~= 0) then
-		local location = inv:get_location()
-		ff.set_speed(player)
-	end
-	--  INVENTORY CODE:
-	--if inv:contains_item("main", "feather_fall:feather") then -- contains_item returns bool
-	--	local location = inv:get_location()
-	--	ff.set_speed(player)
-	--end
-	--  HAND CODE: (original code)
-	--local hand = player:get_wielded_item()
-	--if hand ~= nil then -- Only run if we obtained the player's hand
-	--    -- Check if the player has this mod's feather in their hand
-	--    if hand:get_name() == "feather_fall:feather" then
-	--        ff.set_speed(player)
-	--    end
-	--end
+		local inv = player:get_inventory()
+		if holding_requirement == "hotbar" then
+			local seek_stack = ff.find_stack(inv, "main", "feather_fall:feather")
+			if (seek_stack <= hotbar_slots and seek_stack ~= -1) then
+				local location = inv:get_location()
+				ff.set_speed(player)
+			end
+		elseif holding_requirement == "inventory" then
+			if inv:contains_item("main", "feather_fall:feather") then
+				local location = inv:get_location()
+				ff.set_speed(player)
+			end
+		elseif holding_requirement == "hand" then
+			local hand = player:get_wielded_item()
+			if hand ~= nil then -- Only run if we obtained the player's hand
+				-- Check if the player has this mod's feather in their hand
+				if hand:get_name() == "feather_fall:feather" then
+					ff.set_speed(player)
+				end
+			end
+		end
+		if gravity_reset_timer == 0 then
+			player:set_physics_override({gravity=1})
+		end
     end
 end)
 
